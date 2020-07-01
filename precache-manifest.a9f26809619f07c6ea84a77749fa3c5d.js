d8c1039c0e356825d090b3fdb5afeea639d94966
ColoSRJones <colosrjones@gmail.com>
2020-06-30 21:30:54 -0500

    Updates

---
+++ precache-manifest.a9f26809619f07c6ea84a77749fa3c5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d6711f401416af60a856b24a8ed26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11f8570b2ec842e943c",
+    "url": "/static/css/main.83bc2fc3.chunk.css"
+  },
+  {
+    "revision": "e1767275a455be65d349",
+    "url": "/static/js/2.51193e54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.51193e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11f8570b2ec842e943c",
+    "url": "/static/js/main.f013e889.chunk.js"
+  },
+  {
+    "revision": "48d0cf26010177a6b25b",
+    "url": "/static/js/runtime-main.9b69eca5.js"
+  },
+  {
+    "revision": "1d9bda1a9abf270a09a69ee0dc8a9f74",
+    "url": "/static/media/Youtube-Logo-Header.1d9bda1a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ColoSRJones/youtubeclone/precache-manifest.c82728928f4ba7c900832f3fd82a4f4c.js"
+  "/precache-manifest.a9f26809619f07c6ea84a77749fa3c5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ColoSRJones/youtubeclone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
